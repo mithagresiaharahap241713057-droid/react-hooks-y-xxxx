6d7126fcd4f9e59b4e9b5d3450473990fd5d2369
mithagresiaharahap241713057-droid <mithagresiaharahap241713057@gmail.com>
2026-03-26 11:31:53 +0700

    Update AuthFromWrapper.tsx

--- src/components/AuthFromWrapper.tsx
+++ src/components/AuthFromWrapper.tsx
@@ -1,16 +1,25 @@
 import React, { ReactNode } from 'react';
+
 interface AuthFormWrapperProps {
     title: string;
     children: ReactNode;
 }
-const AuthFromWrapper = ({ title, children }: AuthFormWrapperProps)=> {
+
+const AuthFromWrapper = ({ title, children }: AuthFormWrapperProps) => {
     return (
-        <div className="w-full max-w-md bg-white rounded-xl shadowlg p-8">
-            <h2 className="text-3xl font-cold text-center mb-8 textprimary">
-                {title}
-            </h2>
-            {children}
+        // TAMBAHKAN DIV INI SEBAGAI PEMBUNGKUS LUAR
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-indigo-600 p-4">
+            
+            {/* Kotak Putih Form kamu */}
+            <div className="w-full md:w-[700px] lg:w-[900px] bg-white rounded-2xl shadow-2xl p-10">
+                <h2 className="text-3xl font-bold text-center mb-8 text-primary">
+                    {title}
+                </h2>
+                {children}
+            </div>
+
         </div>
     );
 };
-export default AuthFromWrapper; 
+
+export default AuthFromWrapper;
