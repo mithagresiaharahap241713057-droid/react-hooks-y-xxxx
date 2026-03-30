bd1c9e1bfb5a45943b91f4f5ac62fc534a8aaac0
mithagresiaharahap241713057-droid <mithagresiaharahap241713057@gmail.com>
2026-03-30 11:26:15 +0700

    Update page.tsx
    intial commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,24 @@
-'use client'
-import React from "react";
+'use client';
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Game1 from "../../components/Game1";
+
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const isLogin = localStorage.getItem("isLogin");
+
+        if (!isLogin) {
+            router.push("/auth/not-authorized");
+        }
+    }, []);
+
     return (
-        <div className="flex flex-col items-center justify-center
-min-h-screen min-w-screen">
-            <h1 className="text-4xl font-bold mb-4 textwhite">Selamat Datang!</h1><Game1 />
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold mb-4">Selamat Datang!</h1>
+            <Game1 />
         </div>
     );
 }
